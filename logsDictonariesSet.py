b7f86d5d9c6384187f360d27abebec700ecb67f2
Alejandro Diaz <129427891+DNosheZ@users.noreply.github.com>
2023-09-06 08:14:57 -0500

    Update logsDictonariesSet.py

--- logsDictonariesSet.py
+++ logsDictonariesSet.py
@@ -3,7 +3,7 @@ def logs():
     with open("assets/logdata.txt", "r") as file:
         logReg=[]
         logdata = file.read()
-        log_set=logdata.replace("\n",'*').split("*")
+        log_set=logdata.split("\n")
         pattern = r'^(?P<host>[\d.]+) - (?P<user_name>\w+) \[(?P<request_time>[^\]]+)\] "(?P<request>[^"]+)"'
     for log in log_set:
         match = re.match(pattern, log)
