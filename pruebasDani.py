06216024274ac8072263e0fd9d22d44e13d308d8
Alejandro Diaz <129427891+DNosheZ@users.noreply.github.com>
2023-11-24 11:09:03 -0500

    Update pruebasDani.py

--- pruebasDani.py
+++ pruebasDani.py
@@ -1,7 +1,7 @@
 import random
-cartas={"Jota" : str(f' ___\n|J  |\n| ♦ |\n|__J|\n'),
-"Reina": str(f' ___\n|Q  |\n| ♥ |\n|__Q|\n'),
-"Ocho" : str(f' ___\n|8  |\n| ♣ |\n|__8|\n')
+cartas={"Carta1" : str(f' _\n|J  |\n| ♦ |\n|J|\n'),
+"Carta2": str(f' _\n|Q  |\n| ♥ |\n|Q|\n'),
+"Carta3" : str(f' _\n|8  |\n| ♣ |\n|8|\n')
 }
 items = list(cartas.items())
 random.shuffle(items)
@@ -16,6 +16,15 @@ while carta_a == carta_b:
     carta_b = random.choice(key_list)
 key_list[key_list.index(carta_a)], key_list[key_list.index(carta_b)] = key_list[key_list.index(carta_b)], key_list[key_list.index(carta_a)]
 cartas_shuffle = {key: cartas_shuffle[key] for key in key_list}
+
+#aqui se ordenan las llaves
+# Extract keys and values
+keys = list(cartas_suffle.keys()).sorted()
+values = list(cartas_suffle.values())
+
+# Rebuild the dictionary
+cartas_shuffle = dict(zip(keys, values))
+
 #intercambios.append((carta_a, carta_b))
 print(f"Intercambio {carta_a} con {carta_b}")
 print(cartas_shuffle)
