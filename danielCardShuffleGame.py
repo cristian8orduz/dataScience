59b5e66f80efc94c953b2279065abcfa00d0385d
Alejandro Diaz <129427891+DNosheZ@users.noreply.github.com>
2023-11-23 22:21:53 -0500

    Update danielCardShuffleGame.py

--- danielCardShuffleGame.py
+++ danielCardShuffleGame.py
@@ -1,13 +1,14 @@
 import random
 
 # Inicializar variables
-cartas = ["carta izquierda (I)","carta del medio (M)", "carta derecha (D)"]
-cartas_letras = ["8", "Q", "J"]
-cartas_simbolos = ["♣", "♥", "♦"]
-
+carta1 = " ___\n " + "|J  |\n"+"| ♦ |\n"+"|__J|\n"
+carta2 = " ___\n " + "|Q  |\n"+"| ♥ |\n"+"|__Q|\n"
+carta3 = " ___\n " + "|8  |\n"+"| ♣ |\n"+"|__8|\n"
+cartas_lista=list(carta1,carta2,carta3)
+cartas_den=("Jota","Reina","Ocho")
 def main():
     # con global, se usan las variables declaradas arriba
-    global cartas, cartas_letras, cartas_simbolos
+    global cartasList
     nombre = None
     print("Adivina dónde está la Reina de Corazones")
     while True:        
@@ -18,13 +19,12 @@ def main():
               nombre = input("Por favor, indique su nombre: ")            
             
             # unir listas temporalmente para hacer el shuffle en el mismo orden para letra y símbolo
-            lista_completa = list(zip(cartas, cartas_letras, cartas_simbolos))
+            lista_completa = list(zip(carta_lista, cartas_den))
             random.shuffle(lista_completa)
-            cartas, cartas_letras, cartas_simbolos = zip(*lista_completa)
+            cartas_lista_final, cartas_den_final = zip(*lista_completa)
             # Convertir a listas nuevamente
-            cartas = list(cartas)
-            cartas_letras = list(cartas_letras)
-            cartas_simbolos = list(cartas_simbolos)
+            cartas_lista_final = list(cartas_lista_final)
+            cartas_den_final = list(cartas_den_final)
             
             #print(cartas)
             #print(cartas_letras)
@@ -38,20 +38,20 @@ def main():
             num_intercambios = random.randint(2, 4)
             intercambios = []  
             for _ in range(num_intercambios):
-                carta_a = random.choice(cartas)
-                carta_b = random.choice(cartas)
+                carta_a = random.choice(cartas_den_final)
+                carta_b = random.choice(cartas_den_final)
                 while carta_a == carta_b:                 
-                    carta_b = random.choice(cartas)
-                index_a = cartas.index(carta_a)
-                index_b = cartas.index(carta_b)
+                    carta_b = random.choice(cartas_den_final)
+                index_a = cartas_den_final.index(carta_a)
+                index_b = cartas_den_final.index(carta_b)
                 
                 # intercambiar cartas
-                cartas[index_a], cartas[index_b] = cartas[index_b], cartas[index_a]
+                cartas_den_final[index_a], cartas[index_b] = carta_b, cartas[index_a]
                 # intercambiar letras y símbolos
-                cartas_letras[index_a], cartas_letras[index_b] = cartas_letras[index_b], cartas_letras[index_a]
-                cartas_simbolos[index_a], cartas_simbolos[index_b] = cartas_simbolos[index_b], cartas_simbolos[index_a]
+                cartas_lista_final[index_a], cartas_lista_final[index_b] = cartas_lista_final[index_b], cartas_lista_final[index_a]
                 #intercambios.append((carta_a, carta_b))
                 print(f"Intercambio {carta_a} con {carta_b}")
+                dibujar_cartas()
                 #print(cartas)
 
             # No es necesario, ya que se muestra en el ciclo de arriba
@@ -63,7 +63,7 @@ def main():
             opciones = ["I", "M", "D"]
             respuesta_correcta = int(cartas.index("carta del medio (M)"))
             respuesta = input(f"¿En cuál de las cartas está la Reina de Corazones? [{', '.join(opciones)}]: ")
-            respuesta_index = opciones.index(respuesta)
+            respuesta_index = opciones.index(respuesta)#colocarlo como un entero
 
             if respuesta_index == respuesta_correcta:
                 print("¡Felicidades! Has ganado")
