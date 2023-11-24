2173d926f909e25d400a0e26b919a18ba04cf81e
Alejandro Diaz <129427891+DNosheZ@users.noreply.github.com>
2023-11-24 11:57:40 -0500

    Update pruebasDani.py

--- pruebasDani.py
+++ pruebasDani.py
@@ -1,7 +1,7 @@
 import random
 cartas={"CARTA1" : str(f' _\n|J  |\n| ♦ |\n|J|\n'),
-"CARTA2": str(f' _\n|Q  |\n| ♥ |\n|Q|\n'),
-"CARTA3" : str(f' _\n|8  |\n| ♣ |\n|8|\n')
+        "CARTA2": str(f' _\n|Q  |\n| ♥ |\n|Q|\n'),
+        "CARTA3" : str(f' _\n|8  |\n| ♣ |\n|8|\n')
 }
 items = list(cartas.items())
 random.shuffle(items)
@@ -21,10 +21,8 @@ cartas_shuffle = {key: cartas_shuffle[key] for key in key_list}
 # Extract keys and values
 keys = sorted(list(cartas_shuffle.keys()))
 values = list(cartas_shuffle.values())
-print(keys)
-print(values)
 # Rebuild the dictionary
-#cartas_shuffle = dict(zip(keys, values))
+cartas_shuffle = dict(zip(keys, values))
 
 #intercambios.append((carta_a, carta_b))
 print(f"Intercambio {carta_a} con {carta_b}")
@@ -37,10 +35,7 @@ for key, value in cartas_shuffle.items():
     if value == reina:
         respuesta_correcta = key
         break
-print(respuesta_correcta)
 respuesta = input(f"¿En cuál de las cartas está la Reina de Corazones? [{', '.join(opciones)}]: ").upper()#duda
-print(respuesta)
-print(respuesta_correcta)
 if respuesta == respuesta_correcta:
     print("¡Felicidades! Has ganado")
 else:
